8be4c92ba6ca46c26ce225f1bce467107b2d5b61
doragontoru <torutoru4315@gmail.com>
2025-07-10 15:38:30 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// js/main.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // スムーススクロール
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
